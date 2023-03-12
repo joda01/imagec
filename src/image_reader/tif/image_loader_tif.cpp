d98f57b0b977b44edc476d2df3f6ecdd9f604edd
Joachim Danmayr <danmayr.joachim@fronius.com>
2023-03-12 13:35:14 +0000

    Added some comments

--- src/image_reader/tif/image_loader_tif.cpp
+++ src/image_reader/tif/image_loader_tif.cpp
@@ -1,7 +1,17 @@
-
+#///
+/// \file      image_loader_tif.cpp
+/// \author    Joachim Danmayr
+/// \date      2023-03-12
+///
+/// \copyright Copyright 2019 Joachim Danmayr
+///            All rights reserved! This file is subject
+///            to the terms and conditions defined in file
+///            LICENSE.txt, which is part of this package.
+///
+/// \brief     Allows to open TIFFs and Big Tiffs
+///
 
 #include "image_loader_tif.hpp"
-
 #include <iostream>
 #include <ostream>
 #include <stdexcept>
@@ -15,48 +25,96 @@
 #include <libtiff/tiffio.h>
 #include <libtiff/tiffiop.h>
 
-// http://www.simplesystems.org/libtiff//functions/TIFFCreateDirectory.html
-
+///
+/// \brief      Used to load (very) large TIFF images that cannot be loaded into RAM as a whole.
+///             The prerequisite is that the TIF is stored as a Tiled TIFF.
+///             With this method tile per tile can be loaded and it it possible to load more
+///             than one tile and form a composite image of this tiles.
+///
+///             Loads >nrOfTilesToRead< tiles of a tiled TIFF image starting at >offset<
+///             The tiles are read in squares and put together to form a picture.
+///             Offset is the number of composite tile which should be read.
+///
+///             +-----+-----+-----+-----+-----+-----+-----+
+///             |0    |0    |0    |1    |1    |1    |2    |
+///             |     |     |     |     |     |     |     |
+///             +-----+-----+-----+-----+-----+-----+-----+
+///             |0    |0    |0    |1    |1    |1    |2    |
+///             |     |     |     |     |     |     |     |
+///             +-----+-----+-----+-----+-----+-----+-----+
+///             |0    |0    |0    |1    |1    |1    |2    |
+///             |     |     |     |     |     |     |     |
+///             +-----+-----+-----+-----+-----+-----+-----+
+///             |3    |3    |3    |4    |4    |4    |5    |
+///             |     |     |     |     |     |     |     |
+///             +-----+-----+-----+-----+-----+-----+-----+
+///
+/// \author     Joachim Danmayr
+/// \ref        http://www.simplesystems.org/libtiff//functions.html
+///
+/// \param[in]  filename  Name of the file to load
+/// \param[in]  document  If it is multi document tiff, the index of
+///                       the document which should be loaded
+/// \param[in]  offset    Composite tile number to load
+/// \param[in]  nrOfTilesToRead Nr of tiles which should form one composite image
+/// \return Loaded composite image
+///
 cv::Mat TiffLoader::loadImageTile(const std::string &filename, int document, int offset, int nrOfTilesToRead)
 {
   TIFF *tif = TIFFOpen(filename.c_str(), "r");
   if(tif) {
-    unsigned int width, height, tilewidth, tileheight, tileOffset, tileByteCount;
-    auto cnt1 = TIFFNumberOfDirectories(tif);
+    auto nrOfDirectories = TIFFNumberOfDirectories(tif);
+    if(document >= nrOfDirectories) {
+      throw std::runtime_error("Nr. of directories exceeded!");
+    }
+    // Set the directory to load the image from this directory
     TIFFSetDirectory(tif, document);
 
-    // get the size of the tiff
+    //
+    // Load TIF meta data
+    //
+    unsigned int width, height, tilewidth, tileheight, tileOffset, tileByteCount;
     TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &width);
     TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &height);
-
     TIFFGetField(tif, TIFFTAG_TILEWIDTH, &tilewidth);
     TIFFGetField(tif, TIFFTAG_TILELENGTH, &tileheight);
     TIFFGetField(tif, TIFFTAG_TILEOFFSETS, &tileOffset);
     TIFFGetField(tif, TIFFTAG_TILEBYTECOUNTS, &tileByteCount);
 
+    //
+    // Messy piece of code. But I realized that there are TIFFs where the TIFFTAG_TILELENGTH meta is wrong.
+    // In such a case we assume square tiles using the correct value read from TIFFTAG_TILEWIDTH
+    //
     if(tileheight <= 0) {
       tileheight = tilewidth;
     }
-    uint npixels = tilewidth * tileheight;    // get the total number of pixels
+    uint64_t npixels = tilewidth * tileheight;    // get the total number of pixels per tile
 
+    // Calculate the total number of tiles in x, y direction and the total number of tiles of the whole image
     uint64_t nrOfXTiles = width / tilewidth;
     uint64_t nrOfYTiles = height / tileheight;
-    uint64_t nrOfTilles = nrOfXTiles * nrOfYTiles;
+    uint64_t nrOfTiles  = nrOfXTiles * nrOfYTiles;
 
-    std::cout << "A: " << std::to_string(nrOfXTiles) << "x" << std::to_string(nrOfYTiles) << std::endl;
-
-    // Itterate through all the pixels of the tif
-
+    //
+    // We read squares, therefore calculate the square root of the number of
+    // tiles to read and divide them evenly in x and y direction.
+    // The result is the size of the newly created composite image we get at the end.
+    //
     uint64_t newImageWidth  = tilewidth * std::sqrt(nrOfTilesToRead);
     uint64_t newImageHeight = tileheight * std::sqrt(nrOfTilesToRead);
     cv::Mat image           = cv::Mat(newImageWidth, newImageHeight, CV_8UC3);
 
+    //
+    // Now calculate the number of tiles in x and y direction based on the new image size.
+    // We do this to compensate for rounding errors.
+    //
     uint64_t tileNrX = newImageWidth / tilewidth;
     uint64_t tileNrY = newImageHeight / tileheight;
 
-    std::cout << "TileNr: " << std::to_string(tileNrX) << "x" << std::to_string(tileNrY) << std::endl;
-    std::cout << "ImgSize: " << std::to_string(newImageWidth) << "x" << std::to_string(newImageHeight) << std::endl;
-
+    //
+    // Iterate through all tiles in X and Y directions, load the
+    // Pixel values and write them to an openCV Matrix (Mat).
+    //
     for(int tileOffsetY = 0; tileOffsetY < tileNrY; tileOffsetY++) {
       for(int tileOffsetX = 0; tileOffsetX < tileNrX; tileOffsetX++) {
         //
@@ -65,58 +123,69 @@ cv::Mat TiffLoader::loadImageTile(const std::string &filename, int document, int
         int tilePartX = tileOffsetX * tilewidth;
         int tilePartY = tileOffsetY * tileheight;
 
-        // std::cout << "" << std::to_string(tilePartX) << "x" << std::to_string(tilePartY) << std::endl;
-
         //
-        // Boundarie check
+        // Boundary check
         //
         uint64_t tileToReadX = tilePartX + (offset * tilewidth);
         uint64_t tileToReadY = tilePartY + (offset * tileheight);
-        // std::cout << "" << std::to_string(tileToReadX) << "x" << std::to_string(tileToReadY) << std::endl;
 
         if(tileToReadX > width || tileToReadY > height) {
-          throw std::runtime_error("Offset error");
+          throw std::runtime_error("Tries to read a tile which is not part of the image (array index out of bound)!");
         }
 
-        auto raster = (uint32 *) _TIFFmalloc(
-            npixels * sizeof(uint32));    // allocate temp memory (must use the tiff library malloc)
-        if(raster == NULL)                // check the raster's memory was allocaed
-        {
+        // Allocate temp memory (must use the tiff library malloc)
+        auto raster = (uint32 *) _TIFFmalloc(npixels * sizeof(uint32));
+        if(raster == NULL) {
           TIFFClose(tif);
           throw std::runtime_error("Could not allocate memory for raster of TIFF image.");
         }
+
+        // Read the R(ed), G(reen), B(blue) and A(alpha) channel if one tile of the original image
         if(!TIFFReadRGBATile(tif, tileToReadX, tileToReadY, raster)) {
           TIFFClose(tif);
           throw std::runtime_error("Could not read raster of TIFF image.");
         }
+
+        //
+        // Now it gets tricky: We have to correctly place the read pixel values from the original image
+        // in the openCV image matrix. This is tricky since an openCV image Matrix has its origin (0,0) at the
+        // button left, a TIFF image has it's origin at the top left. Next tricky thing is that images are
+        // rotated counterclockwise and flipped.
+        // We therefore have to write the tiles from the right to left, but the pixels of a tile must written from left
+        // to right. This is done with: >(y + (tilewidth * (tileNrY - tileOffsetY - 1)))<. For the x direction it is
+        // much more easier we can just write them as they are.
+        //
+        // In openCV language: Rotate the image 90 degrees couter clockwise
+        // image = image.t();
+        // cv::flip(image, image, 0);
+        //
         for(uint x = 0; x < tilewidth; x++) {
           for(uint y = 0; y < tileheight; y++) {
-            uint32 &TiffPixel = raster[y * tilewidth + x];    // read the current pixel of the TIF
-            int xImg          = (x + tilePartX);
-            int yImg          = (y + (tilewidth * (tileNrY - tileOffsetY - 1)));
+            uint32 &TiffPixel =
+                raster[y * tilewidth + x];    // Read the current pixel of the TIF (see:
+                                              // http://www.simplesystems.org/libtiff//functions/TIFFReadRGBATile.html))
+            int xImg = (x + tilePartX);
+            int yImg = (y + (tilewidth * (tileNrY - tileOffsetY - 1)));
 
-            cv::Vec3b &pixel = image.at<cv::Vec3b>(cv::Point(yImg, xImg));    // read the current pixel of the matrix
-            pixel[0]         = TIFFGetB(TiffPixel);                           // Set the pixel values as BGRA
-            pixel[1]         = TIFFGetG(TiffPixel);
-            pixel[2]         = TIFFGetR(TiffPixel);
-            // pixel[3]          = TIFFGetA(TiffPixel);
+            cv::Vec3b &pixel = image.at<cv::Vec3b>(
+                cv::Point(yImg, xImg));    // Get the reference of the pixel to write of the opencv matrix
+
+            pixel[0] = TIFFGetB(TiffPixel);    // Set the pixel values as BGRA
+            pixel[1] = TIFFGetG(TiffPixel);
+            pixel[2] = TIFFGetR(TiffPixel);
+            // pixel[3] = TIFFGetA(TiffPixel);   // We ignore the alpha channel since we are using CV_8UC3
           }
         }
-        _TIFFfree(raster);    // release temp memory
+        _TIFFfree(raster);    // Free the allocated memory before next round starts
       }
-      // std::cout << "W: " << std::to_string((tilewidth * (tileNrY - tileOffsetY - 1))) << std::endl;
     }
-    cv::imwrite("out/bigtiff" + std::to_string(98) + ".jpg", image);
 
-    // Rotate the image 90 degrees couter clockwise
-    // image = image.t();
-    // cv::flip(image, image, 0);
-
-    cv::Mat imageOut = cv::Mat(newImageWidth, newImageHeight, CV_32FC3);
-    image.convertTo(imageOut, CV_32FC3);
-
+    // Free the tiff image
     TIFFClose(tif);
 
+    // Convert to a three channel float image, because we need this format for AI
+    cv::Mat imageOut = cv::Mat(newImageWidth, newImageHeight, CV_32FC3);
+    image.convertTo(imageOut, CV_32FC3);
     return imageOut;
   } else {
     throw std::runtime_error("Could not open image!");
@@ -125,7 +194,7 @@ cv::Mat TiffLoader::loadImageTile(const std::string &filename, int document, int
 
 ///
 /// \brief      Load an entire tif image to the RAM and converts it
-///             to an opencv Mat Matric object
+///             to an opencv Mat Matrix object
 /// \author     Joachim Danmayr
 /// \param[in]  filename  Name of the file to load
 /// \param[in]  directory If the image contains multiple imaged,
