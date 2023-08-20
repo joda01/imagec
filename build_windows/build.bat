a9476bfced4dd7900d6096d923d4c5da3300dc0a
Joachim Danmayr <joachim@danmayr.org>
2023-08-20 16:32:55 +0200

    chore: Added RC file

--- build_windows/build.bat
+++ build_windows/build.bat
@@ -1,8 +1,8 @@
 
 rm -f -r ./build
 mkdir ./build
-cmake -S . -DCMAKE_BUILD_TYPE="Debug" -B build
 
-"C:\Program Files\CMake\bin\cmake.EXE" --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=C:\msys64\mingw64\bin\gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:\msys64\mingw64\bin\g++.exe -SC:/Users/Joachim/Documents/github/imagec -Bc:/Users/Joachim/Documents/github/imagec/build -G "MinGW Makefiles"
+
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=C:\msys64\mingw64\bin\gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:\msys64\mingw64\bin\g++.exe -SC:/Users/Joachim/Documents/github/imagec -Bc:/Users/Joachim/Documents/github/imagec/build -G "MinGW Makefiles"
 
 cmake --build build --target imagec --parallel 4
---
+++ build_windows/copy_dll.bat
@@ -0,0 +1,34 @@
+cp C:\msys64\mingw64\bin\libdeflate.dll libdeflate.dll
+cp C:\msys64\mingw64\bin\libgcc_s_seh-1.dll libgcc_s_seh-1.dll
+cp C:\msys64\mingw64\bin\libgfortran-5.dll libgfortran-5.dll
+cp C:\msys64\mingw64\bin\libgomp-1.dll libgomp-1.dll
+cp C:\msys64\mingw64\bin\libIex-3_1.dll libiex-3_1.dll
+cp C:\msys64\mingw64\bin\libIlmThread-3_1.dll libilmthread-3_1.dll
+cp C:\msys64\mingw64\bin\libImath-3_1.dll libimath-3_1.dll
+cp C:\msys64\mingw64\bin\libjbig-0.dll libjbig-0.dll
+cp C:\msys64\mingw64\bin\libjpeg-8.dll libjpeg-8.dll
+cp C:\msys64\mingw64\bin\libLerc.dll liblerc.dll
+cp C:\msys64\mingw64\bin\liblzma-5.dll liblzma-5.dll
+cp C:\msys64\mingw64\bin\libopenblas.dll libopenblas.dll
+cp C:\msys64\mingw64\bin\libopencv_core-407.dll libopencv_core-407.dll
+cp C:\msys64\mingw64\bin\libopencv_dnn-407.dll libopencv_dnn-407.dll
+cp C:\msys64\mingw64\bin\libopencv_imgcodecs-407.dll libopencv_imgcodecs-407.dll
+cp C:\msys64\mingw64\bin\libopencv_imgproc-407.dll libopencv_imgproc-407.dll
+cp C:\msys64\mingw64\bin\libOpenEXR-3_1.dll libopenexr-3_1.dll
+cp C:\msys64\mingw64\bin\libopenjp2-7.dll libopenjp2-7.dll
+cp C:\msys64\mingw64\bin\libpng16-16.dll libpng16-16.dll
+cp C:\msys64\mingw64\bin\libprotobuf.dll libprotobuf.dll 
+cp C:\msys64\mingw64\bin\libpugixml.dll libpugixml.dll
+cp C:\msys64\mingw64\bin\libquadmath-0.dll libquadmath-0.dll
+cp C:\msys64\mingw64\bin\libsharpyuv-0.dll libsharpyuv-0.dll
+cp C:\msys64\mingw64\bin\libstdc++-6.dll libstdc++-6.dll
+cp C:\msys64\mingw64\bin\libtbb12.dll libtbb12.dll
+cp C:\msys64\mingw64\bin\libtiff-6.dll libtiff-6.dll
+cp C:\msys64\mingw64\bin\libwebp-7.dll libwebp-7.dll
+cp C:\msys64\mingw64\bin\libwinpthread-1.dll libwinpthread-1.dll
+cp C:\msys64\mingw64\bin\libzstd.dll libzstd.dll
+cp C:\msys64\mingw64\bin\zlib1.dll zlib1.dll
+
+# cp not found msvcrt.dll
+# cp not found opengl32.dll
+# cp not found user32.dll
