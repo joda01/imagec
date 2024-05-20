e8aec3f7a30de00361ab484088a2d74a8f2f2114
Joachim Danmayr <joachim@danmayr.org>
2024-05-20 11:55:12 +0000

    chore: Removed comment

--- src/backend/results/database/database_test.cpp
+++ src/backend/results/database/database_test.cpp
@@ -93,29 +93,6 @@ TEST_CASE("database:test", "[database_test]")
     joda::log::logInfo("Added image >" + std::to_string(n) + "<");
   }
 
-  /*
-    auto id = DurationCount::start("Insert");
-
-    for(int img = 0; img < 500; img++) {
-      db.addImage(1, img, "My image 1");
-      joda::log::logInfo("Added element >" + std::to_string(img) + "<");
-
-      auto addChannel = [&db, &img](int numOfObjects, const std::string &name, int ch) {
-        db.addChannel(img, ch, name);
-        db.addObject(1, img, ch, numOfObjects);
-      };
-
-      auto id2 = DurationCount::start("Insert ch");
-      addChannel(10000, "EV1", 1);
-      DurationCount::stop(id2);
-
-      addChannel(10000, "EV2", 2);
-      addChannel(1000, "CE1", 3);
-      addChannel(1000, "NUC", 4);
-      addChannel(10000, "COL1", 5);
-      addChannel(10000, "COL2", 6);
-    }
-    */
   db.close();
 
   // DurationCount::stop(id);
