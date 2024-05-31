8b98099f7bbd863b033805c2eb57b5c4c428227d
Joachim Danmayr <joachim@danmayr.org>
2024-05-31 16:23:40 +0000

    chore: DB fix

--- build_duckdb.ps1
+++ build_duckdb.ps1
@@ -1,6 +1,6 @@
 
 
-git clone -b v0.20.2 https://github.com/duckdb/duckdb.git
+git clone -b v0.10.2 https://github.com/duckdb/duckdb.git
 cd duckdb
 cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DBUILD_EXTENSIONS=parquet -DOVERRIDE_GIT_DESCRIBE="v0.10.2"
 cmake --build . --config Release
