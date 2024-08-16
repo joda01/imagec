f23f6f89fbd09ffaaf7d23efe4193e3a14bbc999
Joachim Danmayr <joachim@danmayr.org>
2024-08-16 11:43:09 +0000

    chore: Improved image save functions

--- src/backend/commands/image_functions/image_saver/image_saver.hpp
+++ src/backend/commands/image_functions/image_saver/image_saver.hpp
@@ -16,6 +16,7 @@
 #include <bits/iterator_concepts.h>
 #include <filesystem>
 #include <string>
+#include "backend/artifacts/roi/roi.hpp"
 #include "backend/commands/command.hpp"
 #include "backend/commands/image_functions/image_saver/image_saver_settings.hpp"
 #include "backend/helper/duration_count/duration_count.h"
@@ -59,14 +60,101 @@ public:
          mSettings.namePrefix + ".png");
 
     // Convert to 8-bit grayscale
-    cv::Mat img_8bit_gray;
-    image.convertTo(img_8bit_gray, CV_8U, 1.0 / 256);    // Scale down to 8-bit
     cv::Mat img_8bit_color;
-    cvtColor(img_8bit_gray, img_8bit_color, cv::COLOR_GRAY2BGR);
-    cv::imwrite(saveName.string(), img_8bit_color);
+
+    // Paint on existing image
+    if(mSettings.canvas == settings::ImageSaverSettings::Canvas::IMAGE_PLANE ||
+       mSettings.canvas == settings::ImageSaverSettings::Canvas::IMAGE_$) {
+      cv::Mat &imageBackground = image;
+      if(mSettings.canvas == settings::ImageSaverSettings::Canvas::IMAGE_PLANE && mSettings.planesIn.has_value()) {
+        imageBackground = context.loadImageFromCache(mSettings.planesIn.value())->image;
+      }
+
+      cv::Mat img_8bit_gray;
+      imageBackground.convertTo(img_8bit_gray, CV_8U, 1.0 / 256);    // Scale down to 8-bit
+      cvtColor(img_8bit_gray, img_8bit_color, cv::COLOR_GRAY2BGR);
+    } else if(mSettings.canvas == settings::ImageSaverSettings::Canvas::BLACK) {
+      img_8bit_color = cv::Mat::zeros(context.getImageSize(), CV_8UC3);
+    } else if(mSettings.canvas == settings::ImageSaverSettings::Canvas::WHITE) {
+      img_8bit_color = cv::Mat(context.getImageSize(), CV_8UC3, cv::Scalar(255, 255, 255));
+    }
+
+    //
+    // Paint the objects
+    //
+    for(const auto &cluster : mSettings.clustersIn) {
+      const auto &clusterObjects = result.at(context.getClusterId(cluster.clusterIn));
+
+      for(const auto &roi : clusterObjects) {
+        for(const auto &classIn : cluster.classesIn) {
+          drawObject(classIn, roi, img_8bit_color);
+        }
+      }
+    }
+
+    std::vector<int> compression_params;
+    compression_params.push_back(cv::IMWRITE_PNG_COMPRESSION);
+    compression_params.push_back(mSettings.compression);
+    cv::imwrite(saveName.string(), img_8bit_color, compression_params);
   }
 
 private:
+  /////////////////////////////////////////////////////
+  static inline int THICKNESS     = 1;
+  static inline cv::Scalar BLACK  = cv::Scalar(0, 0, 0);
+  static inline cv::Scalar WHITE  = cv::Scalar(255, 255, 255);
+  static inline cv::Scalar YELLOW = cv::Scalar(0, 255, 255);
+  static inline cv::Scalar RED    = cv::Scalar(0, 0, 255);
+  static inline cv::Scalar GREEN  = cv::Scalar(0, 255, 0);
+
+  /////////////////////////////////////////////////////
+  void drawObject(const settings::ImageSaverSettings::Cluster::Class &settings, const atom::ROI &roi, cv::Mat &imageOut)
+  {
+    if(settings.classIn == roi.getClassId()) {
+      int left   = roi.getBoundingBox().x;
+      int top    = roi.getBoundingBox().y;
+      int width  = roi.getBoundingBox().width;
+      int height = roi.getBoundingBox().height;
+
+      if(!roi.getMask().empty() && !roi.getBoundingBox().empty()) {
+        auto areaColor = hexToScalar(settings.color);
+
+        // Boundding box
+        if(settings.paintBoundingBox && !roi.getBoundingBox().empty()) {
+          rectangle(imageOut, roi.getBoundingBox(), areaColor, 1 * THICKNESS, cv::LINE_4);
+        }
+
+        // Fill area
+        if(settings.style == settings::ImageSaverSettings::Cluster::Class::Style::FILLED) {
+          imageOut(roi.getBoundingBox()).setTo(areaColor, roi.getMask());
+        }
+
+        // Paint contour only for valid particles
+        cv::Scalar contourColor = hexToScalar(settings.color);
+        std::vector<std::vector<cv::Point>> contours;
+        contours.push_back(roi.getContour());
+        if(!contours.empty()) {
+          drawContours(imageOut(roi.getBoundingBox()), contours, -1, contourColor, 1);
+        }
+
+        if(roi.hasSnapArea()) {
+          std::vector<std::vector<cv::Point>> contours;
+          contours.push_back(roi.getSnapAreaContour());
+          if(!contours.empty())
+            drawContours(imageOut(roi.getSnapAreaBoundingBox()), contours, -1, contourColor, 1);
+        }
+      }
+    }
+  }
+
+  cv::Scalar hexToScalar(const std::string &hexColor)
+  {
+    int r, g, b;
+    sscanf(hexColor.c_str(), "#%2x%2x%2x", &r, &g, &b);
+    return cv::Scalar(b, g, r);    // OpenCV uses BGR order
+  }
+
+  /////////////////////////////////////////////////////
   const settings::ImageSaverSettings &mSettings;
 };
 
--- src/backend/commands/image_functions/image_saver/image_saver_settings.hpp
+++ src/backend/commands/image_functions/image_saver/image_saver_settings.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
 #include <cstdint>
+#include <optional>
 #include <set>
+#include "backend/enums/enum_images.hpp"
+#include "backend/enums/enums_classes.hpp"
+#include "backend/enums/enums_clusters.hpp"
+#include "backend/helper/json_optional_parser_helper.hpp"
 #include "backend/settings/setting.hpp"
 #include <nlohmann/json.hpp>
 
@@ -10,21 +15,92 @@ namespace joda::settings {
 struct ImageSaverSettings
 {
 public:
+  enum class Canvas
+  {
+    WHITE,
+    BLACK,
+    IMAGE_$,
+    IMAGE_PLANE
+  };
+
+  struct Cluster
+  {
+    struct Class
+    {
+      enum class Style
+      {
+        OUTLINED,
+        FILLED
+      };
+
+      enums::ClassId classIn = enums::ClassId::UNDEFINED;
+      std::string color      = "#FF0000";
+      Style style            = Style::OUTLINED;
+      bool paintBoundingBox  = false;
+
+      void check() const
+      {
+      }
+
+      NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT_EXTENDED(Class, classIn, color, style, paintBoundingBox);
+    };
+
+    enums::ClusterIdIn clusterIn = enums::ClusterIdIn::$;
+    std::vector<Class> classesIn;
+
+    void check() const
+    {
+    }
+
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT_EXTENDED(Cluster, clusterIn, classesIn);
+  };
+
+  std::vector<Cluster> clustersIn;
+
   //
   // PNG compression level (0 = no compression)
   //
-  int32_t compression = 0;
+  int32_t compression = 1;
 
   //
   // Image name prefix
   //
   std::string namePrefix;
 
+  //
+  // Where should the output be painted on
+  //
+  Canvas canvas = Canvas::IMAGE_$;
+
+  //
+  // On which image it should be painter
+  //
+  std::optional<enums::ImageId> planesIn = std::nullopt;
+
   /////////////////////////////////////////////////////
   void check() const
   {
+    if(canvas == ImageSaverSettings::Canvas::IMAGE_PLANE) {
+      CHECK(planesIn.has_value(), "Define a plane on which the output should be painted!");
+    }
+    CHECK(compression >= 0 && compression <= 6, "Image compression must be in between [0-6].");
   }
 
-  NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT_EXTENDED(ImageSaverSettings, compression, namePrefix);
+  NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT_EXTENDED(ImageSaverSettings, canvas, planesIn, compression, namePrefix,
+                                                       clustersIn);
 };
+
+NLOHMANN_JSON_SERIALIZE_ENUM(ImageSaverSettings::Cluster::Class::Style,
+                             {
+                                 {ImageSaverSettings::Cluster::Class::Style::OUTLINED, "Outlined"},
+                                 {ImageSaverSettings::Cluster::Class::Style::FILLED, "Filled"},
+                             });
+
+NLOHMANN_JSON_SERIALIZE_ENUM(ImageSaverSettings::Canvas, {
+                                                             {ImageSaverSettings::Canvas::BLACK, "Black"},
+                                                             {ImageSaverSettings::Canvas::WHITE, "White"},
+                                                             {ImageSaverSettings::Canvas::IMAGE_$, "$"},
+                                                             {ImageSaverSettings::Canvas::IMAGE_PLANE, "ImagePlane"},
+                                                         });
+
 }    // namespace joda::settings
