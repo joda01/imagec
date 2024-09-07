4aa01833f30e5e555d60a017939e6223a66a9082
Joachim Danmayr <joachim@danmayr.org>
2024-09-07 14:19:47 +0000

    fix: Segfault on lodaing wrong time

--- src/backend/helper/helper.hpp
+++ src/backend/helper/helper.hpp
@@ -99,7 +99,10 @@ inline auto stringToNumber(const std::string &str) -> int
 inline std::string timepointToIsoString(const std::chrono::system_clock::time_point &tp)
 {
   std::time_t t = std::chrono::system_clock::to_time_t(tp);
-  std::tm tm    = *std::gmtime(&t);    // Get UTC time
+  if(t < 0) {
+    return "";
+  }
+  std::tm tm = *std::gmtime(&t);    // Get UTC time
   char buffer[80];
   std::strftime(buffer, 80, "%Y-%m-%dT%H-%M-%S", &tm);
   auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()) % 1000;
