17d83edee9d42848bfd5384bb695e4cc837db241
Joachim Danmayr <danmayr.joachim@fronius.com>
2024-09-14 19:09:56 +0000

    chore: Removed conan lock

--- install_conan.sh
+++
@@ -1,6 +0,0 @@
-conan profile detect --force
-conan install . --profile conan/profile_linux --output-folder=build --build=missing
-cd build
-
-cmake .. -G "Unix Makefiles" -DTAG_NAME="$TAG_NAME" -DCMAKE_BUILD_TYPE="Release" -DCMAKE_TOOLCHAIN_FILE="build/Release/generators/conan_toolchain.cmake"
-cmake --build . --config Release
