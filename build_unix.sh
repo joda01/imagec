e3c5211e25ed01d2c2d720764b8053ce859e5bf2
Joachim Danmayr <danmayr.joachim@fronius.com>
2024-09-15 07:44:51 +0000

    chore: Do not pass build type

--- build_unix.sh
+++ build_unix.sh
@@ -1,3 +1,3 @@
 cd build
-cmake .. -G "Unix Makefiles" -DTAG_NAME="$TAG_NAME" -DCMAKE_BUILD_TYPE="Release" -DCMAKE_TOOLCHAIN_FILE="build/Release/generators/conan_toolchain.cmake"
+cmake .. -G "Unix Makefiles" -DTAG_NAME="$TAG_NAME" -DCMAKE_TOOLCHAIN_FILE="build/Release/generators/conan_toolchain.cmake"
 cmake --build . --config Release --target imagec --parallel 8
