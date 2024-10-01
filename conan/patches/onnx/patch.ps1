9fbdff4b10a358885b21e8742126a03338ca0ea7
Joachim Danmayr <46405460+joda01@users.noreply.github.com>
2024-10-01 07:23:00 +0200

    Update patch.ps1

--- conan/patches/onnx/patch.ps1
+++ conan/patches/onnx/patch.ps1
@@ -2,12 +2,6 @@ conan download "onnx/1.16.2" -r conancenter --only-recipe
 
 $onnxPatchPath = "C:\Users\runneradmin\.conan2\p\onnx305a97e88c569\s"
 $conanmetaPath = "C:\Users\runneradmin\.conan2\p\onnx305a97e88c569\e\conandata.yml"
-$onnxSource = "C:\Users\runneradmin\.conan2\p\onnx305a97e88c569\s\src.tar.gz"
-$unpackesFolderName = "C:\Users\runneradmin\.conan2\p\onnx305a97e88c569\s\onnx-1.16.2"
-
-Invoke-WebRequest -Uri "https://github.com/onnx/onnx/archive/v1.16.2.tar.gz" -OutFile $onnxSource
-tar -xvzf $onnxSource -C $onnxPatchPath
-Rename-Item $unpackesFolderName "src"
 
 if (-not (Test-Path -Path $onnxPatchPath)) {
     New-Item -Path $onnxPatchPath -ItemType Directory
@@ -15,6 +9,13 @@ if (-not (Test-Path -Path $onnxPatchPath)) {
 } else {
     Write-Output "Folder already exists!"
 }
+
+$onnxSource = "C:\Users\runneradmin\.conan2\p\onnx305a97e88c569\s\src.tar.gz"
+$unpackesFolderName = "C:\Users\runneradmin\.conan2\p\onnx305a97e88c569\s\onnx-1.16.2"
+
+Invoke-WebRequest -Uri "https://github.com/onnx/onnx/archive/v1.16.2.tar.gz" -OutFile $onnxSource
+tar -xvzf $onnxSource -C $onnxPatchPath
+Rename-Item $unpackesFolderName "src"
 
 Copy-Item -Path conandata.yml -Destination $conanmetaPath -Force
 Copy-Item -Recurse -Path patches -Destination $onnxPatchPath -Force
