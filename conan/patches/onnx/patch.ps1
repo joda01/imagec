db192c64ffb2917879841c6b3d307849e9098643
Joachim Danmayr <46405460+joda01@users.noreply.github.com>
2024-10-01 07:03:02 +0200

    chore: Download source

--- conan/patches/onnx/patch.ps1
+++ conan/patches/onnx/patch.ps1
@@ -2,6 +2,12 @@ conan download "onnx/1.16.2" -r conancenter --only-recipe
 
 $onnxPatchPath = "C:\Users\runneradmin\.conan2\p\onnx305a97e88c569\s"
 $conanmetaPath = "C:\Users\runneradmin\.conan2\p\onnx305a97e88c569\e\conandata.yml"
+$onnxSource = "C:\Users\runneradmin\.conan2\p\onnx305a97e88c569\s\src.tar.gz"
+$unpackesFolderName = "C:\Users\runneradmin\.conan2\p\onnx305a97e88c569\s\onnx-1.16.2"
+
+Invoke-WebRequest -Uri "https://github.com/onnx/onnx/archive/v1.16.2.tar.gz" -OutFile $onnxSource
+tar -xvzf $onnxSource -C $onnxPatchPath
+Rename-Item $unpackesFolderName "src"
 
 if (-not (Test-Path -Path $onnxPatchPath)) {
     New-Item -Path $onnxPatchPath -ItemType Directory
