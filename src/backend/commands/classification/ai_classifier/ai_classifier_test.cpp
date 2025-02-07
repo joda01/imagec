e00b92be81f4062ce2b6ffbb9306bb28d5f91275
Joachim Danmayr <joachim@danmayr.org>
2025-02-07 21:14:18 +0000

    chore: Added unit test

--- src/backend/commands/classification/ai_classifier/ai_classifier_test.cpp
+++ src/backend/commands/classification/ai_classifier/ai_classifier_test.cpp
@@ -15,9 +15,8 @@
 /// \brief  Load a config file
 /// \author Joachim Danmayr
 ///
-TEST_CASE("ai::classifier::test::nucleus", "[.][ai_classifieer]")
+TEST_CASE("ai::classifier::test::nucleus", "[ai_classifier]")
 {
-  joda::image::reader::ImageReader::init(1e9);
   auto omeXML = joda::image::reader::ImageReader::getOmeInformation("models/nucleisegmentationboundarymodel_torchscript/sample_input_00.tif", 0);
   auto img = joda::image::reader::ImageReader::loadEntireImage("models/nucleisegmentationboundarymodel_torchscript/sample_input_00.tif", {0, 0, 0}, 0,
                                                                0, omeXML);
@@ -50,12 +49,12 @@ TEST_CASE("ai::classifier::test::nucleus", "[.][ai_classifieer]")
   joda::cmd::ImageSaver imgSaver(imageSaver);
   imgSaver.execute(context, img, result);
 
-  joda::image::reader::ImageReader::destroy();
+  CHECK(result.size() == 1);
+  CHECK(result.begin()->second->size() == 44);
 }
 
-TEST_CASE("ai::classifier::test::livecell", "[.][ai_classifieer]")
+TEST_CASE("ai::classifier::test::livecell", "[ai_classifier]")
 {
-  joda::image::reader::ImageReader::init(1e9);
   auto omeXML = joda::image::reader::ImageReader::getOmeInformation("models/livecellsegmentationboundarymodel_torchscript/sample_input_0.tif", 0);
   auto img = joda::image::reader::ImageReader::loadEntireImage("models/livecellsegmentationboundarymodel_torchscript/sample_input_0.tif", {0, 0, 0},
                                                                0, 0, omeXML);
@@ -100,70 +99,19 @@ TEST_CASE("ai::classifier::test::livecell", "[.][ai_classifieer]")
   joda::cmd::ImageSaver imgSaver(imageSaver);
   imgSaver.execute(context, img, result);
 
-  joda::image::reader::ImageReader::destroy();
-}
-
-TEST_CASE("ai::classifier::test::livecell::sbg", "[.][ai_classifieer]")
-{
-  std::string path = "/workspaces/imagec/build/build/output/Test ImageC v15/test folder/B2_15_5ADVMLE.vsi.vsi";
-
-  joda::image::reader::ImageReader::init(1e9);
-  auto omeXML = joda::image::reader::ImageReader::getOmeInformation(path, 0);
-  auto img    = joda::image::reader::ImageReader::loadEntireImage(path, {0, 3, 0}, 0, 0, omeXML);
-
-  joda::settings::AiClassifierSettings aiSets;
-  aiSets.modelPath = "models/livecellsegmentationboundarymodel_torchscript/weights-torchscript.pt";
-  aiSets.modelClasses.emplace_back(joda::settings::ObjectClass{.modelClassId = 0});
-  aiSets.modelClasses.emplace_back(joda::settings::ObjectClass{.modelClassId = 1});
-  aiSets.modelClasses.emplace_back(joda::settings::ObjectClass{.modelClassId = 2});
-  aiSets.modelClasses.emplace_back(joda::settings::ObjectClass{.modelClassId = 3});
-  aiSets.modelClasses.emplace_back(joda::settings::ObjectClass{.modelClassId = 4});
-  aiSets.modelClasses.emplace_back(joda::settings::ObjectClass{.modelClassId = 5});
-
-  aiSets.modelInputParameters = joda::onnx::OnnxParser::getModelInfo("models/livecellsegmentationboundarymodel_torchscript/weights-torchscript.pt");
-
-  joda::cmd::AiClassifier ai(aiSets);
-  joda::atom::ObjectList result;
-  joda::settings::ProjectImageSetup setup;
-  joda::processor::PipelineInitializer pipeLinieInit(setup);
-  joda::processor::GlobalContext glob;
-  glob.resultsOutputFolder = "/workspaces/imagec/tmp";
-  joda::processor::PlateContext plate;
-  joda::processor::ImageContext imgCtx{pipeLinieInit, path, omeXML};
-  joda::processor::IterationContext iter;
-  joda::processor::ProcessContext context(glob, plate, imgCtx, iter);
-
-  ai.execute(context, img, result);
-  joda::settings::ImageSaverSettings imageSaver;
-  imageSaver.classesIn.emplace_back(joda::settings::ImageSaverSettings::SaveClasss{
-      .inputClass = joda::enums::ClassIdIn::C0, .style = joda::settings::ImageSaverSettings::Style::OUTLINED, .paintBoundingBox = false});
-  imageSaver.classesIn.emplace_back(joda::settings::ImageSaverSettings::SaveClasss{
-      .inputClass = joda::enums::ClassIdIn::C1, .style = joda::settings::ImageSaverSettings::Style::OUTLINED, .paintBoundingBox = false});
-  imageSaver.classesIn.emplace_back(joda::settings::ImageSaverSettings::SaveClasss{.inputClass = joda::enums::ClassIdIn::C2,
-                                                                                   .style = joda::settings::ImageSaverSettings::Style::OUTLINED});
-  imageSaver.classesIn.emplace_back(joda::settings::ImageSaverSettings::SaveClasss{.inputClass = joda::enums::ClassIdIn::C3,
-                                                                                   .style      = joda::settings::ImageSaverSettings::Style::FILLED});
-  imageSaver.classesIn.emplace_back(joda::settings::ImageSaverSettings::SaveClasss{.inputClass = joda::enums::ClassIdIn::C4,
-                                                                                   .style      = joda::settings::ImageSaverSettings::Style::FILLED});
-  imageSaver.classesIn.emplace_back(joda::settings::ImageSaverSettings::SaveClasss{.inputClass = joda::enums::ClassIdIn::C5,
-                                                                                   .style      = joda::settings::ImageSaverSettings::Style::FILLED});
-
-  joda::cmd::ImageSaver imgSaver(imageSaver);
-  imgSaver.execute(context, img, result);
-
-  joda::image::reader::ImageReader::destroy();
+  CHECK(result.size() == 1);
+  CHECK(result.begin()->second->size() == 193);
 }
 
 ///
 /// \brief  Load a config file
 /// \author Joachim Danmayr
 ///
-TEST_CASE("ai::classifier::test::onnx", "[.][ai_classifieer_onnx]")
+TEST_CASE("ai::classifier::test::onnx", "[ai_classifier]")
 {
   std::string path = "/workspaces/imagec/build/build/output/Test ImageC v15/test folder/B2_15_5ADVMLE.vsi.vsi";
-  joda::image::reader::ImageReader::init(1e8);
-  auto omeXML = joda::image::reader::ImageReader::getOmeInformation(path, 0);
-  auto img    = joda::image::reader::ImageReader::loadEntireImage(path, {0, 3, 0}, 0, 0, omeXML);
+  auto omeXML      = joda::image::reader::ImageReader::getOmeInformation(path, 0);
+  auto img         = joda::image::reader::ImageReader::loadEntireImage(path, {0, 3, 0}, 0, 0, omeXML);
 
   joda::settings::AiClassifierSettings aiSets;
   aiSets.modelPath = "models/university_of_sbg_cell_segmentation_v3/weights.onnx";
@@ -193,15 +141,15 @@ TEST_CASE("ai::classifier::test::onnx", "[.][ai_classifieer_onnx]")
   joda::cmd::ImageSaver imgSaver(imageSaver);
   imgSaver.execute(context, img, result);
 
-  joda::image::reader::ImageReader::destroy();
+  CHECK(result.size() == 1);
+  CHECK(result.begin()->second->size() == 56);
 }
 
-TEST_CASE("ai::classifier::test::pytorch::yolo", "[.][ai_classifieer_onnx]")
+TEST_CASE("ai::classifier::test::pytorch::yolo", "[ai_classifier]")
 {
   std::string path = "/workspaces/imagec/build/build/output/Test ImageC v15/test folder/B2_15_5ADVMLE.vsi.vsi";
-  joda::image::reader::ImageReader::init(1e8);
-  auto omeXML = joda::image::reader::ImageReader::getOmeInformation(path, 0);
-  auto img    = joda::image::reader::ImageReader::loadEntireImage(path, {0, 3, 0}, 0, 0, omeXML);
+  auto omeXML      = joda::image::reader::ImageReader::getOmeInformation(path, 0);
+  auto img         = joda::image::reader::ImageReader::loadEntireImage(path, {0, 3, 0}, 0, 0, omeXML);
 
   joda::settings::AiClassifierSettings aiSets;
   aiSets.modelPath = "models/university_of_sbg_cell_segmentation_v3/weights.pt";
@@ -232,5 +180,6 @@ TEST_CASE("ai::classifier::test::pytorch::yolo", "[.][ai_classifieer_onnx]")
   joda::cmd::ImageSaver imgSaver(imageSaver);
   imgSaver.execute(context, img, result);
 
-  joda::image::reader::ImageReader::destroy();
+  CHECK(result.size() == 1);
+  CHECK(result.begin()->second->size() == 56);
 }
