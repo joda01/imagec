4f440afe4b98c14b55c82ff7c7385658d1f219a0
Joachim Danmayr <joachim@danmayr.org>
2025-02-09 07:27:11 +0000

    chore: REmoved long jumps

--- src/backend/helper/rapidyaml/rapidyaml.cpp
+++ src/backend/helper/rapidyaml/rapidyaml.cpp
@@ -13,6 +13,7 @@
 
 #include <ryml.hpp>
 
+#include <exception>
 #include <iostream>
 #include <stdexcept>
 #include "backend/helper/helper.hpp"
@@ -34,7 +35,7 @@ struct ErrorHandlerExample
   C4_NORETURN void on_error(const char *msg, size_t len, ryml::Location loc);
   C4_NORETURN static void s_error(const char *msg, size_t len, ryml::Location loc, void *this_);
   template <class Fn>
-  C4_NODISCARD bool check_error_occurs(Fn &&fn) const;
+  C4_NODISCARD std::string check_error_occurs(Fn &&fn) const;
   template <class Fn>
   C4_NODISCARD bool check_assertion_occurs(Fn &&fn) const;
   void check_effect(bool committed) const;
@@ -46,18 +47,15 @@ struct ErrorHandlerExample
 
 // checking
 template <class Fn>
-C4_NODISCARD bool ErrorHandlerExample::check_error_occurs(Fn &&fn) const
+C4_NODISCARD std::string ErrorHandlerExample::check_error_occurs(Fn &&fn) const
 {
-  bool expected_error_occurred = false;
-  C4_IF_EXCEPTIONS_(try, if(setjmp(s_jmp_env) == 0))    // selectively picks based on availability of exceptions
-  {
+  try {
     fn();
+  } catch(const std::exception &ex) {
+    return std::string(">" + std::string(ex.what()) + "<");
   }
-  C4_IF_EXCEPTIONS_(catch(...), else)
-  {
-    expected_error_occurred = true;
-  }
-  return expected_error_occurred;
+
+  return "";
 }
 template <class Fn>
 C4_NODISCARD bool ErrorHandlerExample::check_assertion_occurs(Fn &&fn) const
@@ -79,15 +77,8 @@ C4_NORETURN void ErrorHandlerExample::s_error(const char *msg, size_t len, ryml:
 /** this is the where the callback implementation goes. Remember that it must not return. */
 C4_NORETURN void ErrorHandlerExample::on_error(const char *msg, size_t len, ryml::Location loc)
 {
-  std::string full_msg = ryml::formatrs<std::string>("{}:{}:{} ({}B): ERROR: {}", loc.name, loc.line, loc.col, loc.offset, ryml::csubstr(msg, len));
-  std::cout << full_msg << std::endl;
-  C4_IF_EXCEPTIONS(
-      // this will execute if exceptions are enabled.
-      throw std::runtime_error(full_msg);,
-                                         // this will execute if exceptions are disabled. It will
-                                         // jump to the function calling the corresponding setjmp().
-                                         s_jmp_msg = full_msg;
-      std::longjmp(s_jmp_env, 1););
+  auto full_msg = ryml::formatrs<std::string>("{}:{}:{} ({}B): ERROR: {}", loc.name, loc.line, loc.col, loc.offset, ryml::csubstr(msg, len));
+  throw std::runtime_error(full_msg);
 }
 
 /** a helper to create the Callbacks object with the custom error handler */
@@ -112,18 +103,19 @@ nlohmann::json Yaml::convert(std::string yamlString)
   nlohmann::json out;
   ryml::set_callbacks(errh.callbacks());
   errh.check_effect(/*committed*/ true);
-  if(errh.check_error_occurs([&] {
-       joda::helper::stringReplace(yamlString, "- inf", "- 'inf'");
-       ryml::Tree tree        = ryml::parse_in_place(yamlString.data());
-       std::string jsonString = ryml::emitrs_json<std::string>(tree);
-       try {
-         out = nlohmann::json::parse(jsonString);
-       } catch(...) {
-         std::cout << jsonString << std::endl;
-       }
-     })) {
-    // throw std::runtime_error("Error parsing error!");
-  } else {
+  auto ret = errh.check_error_occurs([&] {
+    joda::helper::stringReplace(yamlString, "- inf", "- 'inf'");
+    ryml::Tree tree = ryml::parse_in_place(yamlString.data());
+    auto jsonString = ryml::emitrs_json<std::string>(tree);
+    try {
+      out = nlohmann::json::parse(jsonString);
+    } catch(const std::string &ex) {
+      throw std::runtime_error("Json parsing error >" + jsonString + "<");
+    }
+  });
+
+  if(!ret.empty()) {
+    throw std::runtime_error(ret);
   }
   ryml::set_callbacks(errh.defaults);    // restore defaults.
   errh.check_effect(/*committed*/ false);
