3ffca7ef8ac6eeaf93393e8389fc5f3fc687f816
Joachim Danmayr <joachim@danmayr.org>
2025-03-14 18:44:11 +0000

    fix: Added missing shared lib to loinux build

--- resources/launcher/imagec.sh
+++ resources/launcher/imagec.sh
@@ -7,8 +7,15 @@ tmp="${dirname#?}"
 if [ "${dirname%$tmp}" != "/" ]; then
 dirname=$PWD/$dirname
 fi
+
+QT_QPA_PLATFORM_PLUGIN_PATH=$dirname/plugins/platforms
+export QT_QPA_PLATFORM_PLUGIN_PATH
+
+#offscreen
+QT_QPA_PLATFORM="xcb"
+export QT_QPA_PLATFORM
+
 LD_LIBRARY_PATH=$dirname/lib
 export LD_LIBRARY_PATH
-
 
 $dirname/$appname "$@"
