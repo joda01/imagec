1d45993f6e65a1c7852af73fa6bf39cd9a94e430
Joachim Danmayr <joachim@danmayr.org>
2025-08-06 13:56:36 +0000

    chore: Links without cuda

--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,4 @@
+import os
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
 from conan.tools.cmake import CMakeToolchain, CMakeDeps
@@ -5,9 +6,11 @@ from conan.tools.cmake import CMake, cmake_layout
 
 
 
+
+
 class ImageC(ConanFile):
-    name = "ImageC"
-    version = "1.0-beta"
+    name = "imagec"
+    version = "1.0.0"
     license = ["AGPL-3.0","imagec"]
     author = "Joachim Danmayr <support@imagec.org>"
     url = "https://github.com/joda01/imagec"
@@ -20,11 +23,14 @@ class ImageC(ConanFile):
         "with_cuda": [True, False]
     }
     default_options = {
-        "with_cuda": True
+        "with_cuda": False
     }
 
     exports_sources = "src/*"
-    
+
+    def set_version(self):
+        self.version = self.version
+
     def requirements(self):
         self.requires("qt/6.7.1", force=True)
         self.requires("opencv/4.10.0")
@@ -39,10 +45,10 @@ class ImageC(ConanFile):
         self.requires("onnx/1.17.0", force=True)
         self.requires("rapidyaml/0.7.1")
         self.requires("onnxruntime/1.18.1")
-        #if self.options.with_cuda:
-        #    self.requires("libtorch/2.7.1", options={"with_cuda": True})
-        #else:
-        #    self.requires("libtorch/2.7.1", options={"with_cuda": False})
+        if self.options.with_cuda:
+            self.requires("libtorch/2.7.1", options={"with_cuda": True})
+        else:
+            self.requires("libtorch/2.7.1", options={"with_cuda": False})
         self.requires("tensorflow-lite/2.15.0")
         self.requires("flatbuffers/23.5.26", force=True)
         self.requires("protobuf/3.21.12", override=True)
@@ -55,11 +61,12 @@ class ImageC(ConanFile):
         #self.requires("cpp-httplib/0.19.0")
 
     def generate(self):
+        print("=====GENERATE=========")
         deps = CMakeDeps(self)
         deps.generate()
         toolchain = CMakeToolchain(self)
         toolchain.variables["WITH_CUDA"] = self.options.with_cuda
-        toolchain.variables["TAG_NAME"] = "devel"
+        toolchain.variables["TAG_NAME"] = "1.0.0"
         toolchain.generate()
 
     def build(self):
@@ -72,3 +79,4 @@ class ImageC(ConanFile):
 
 
 # conan build . --profile conan/profile_linux
+# conan remote disable imageclibs
--- src/backend/commands/classification/ai_classifier/ai_classifier.cpp
+++ src/backend/commands/classification/ai_classifier/ai_classifier.cpp
@@ -11,7 +11,6 @@
 ///\link       https :// github.com/UNeedCryDear/yolov5-seg-opencv-onnxruntime-cpp
 
 #include "ai_classifier.hpp"
-#include <c10/cuda/CUDACachingAllocator.h>
 #include <exception>
 #include <filesystem>
 #include <memory>
@@ -21,8 +20,8 @@
 #include "backend/commands/classification/ai_classifier/frameworks/onnx/ai_classifier_onnx.hpp"
 #include "backend/commands/classification/ai_classifier/frameworks/pytorch/ai_classifier_pytorch.hpp"
 #include "backend/commands/classification/ai_classifier/models/ai_model.hpp"
-#include "backend/commands/classification/ai_classifier/models/unet/ai_model_unet.hpp"
 #include "backend/commands/classification/ai_classifier/models/cyto3/ai_model_cyto3.hpp"
+#include "backend/commands/classification/ai_classifier/models/unet/ai_model_unet.hpp"
 #include "backend/commands/classification/ai_classifier/models/yolo/ai_model_yolo.hpp"
 #include "backend/enums/enum_objects.hpp"
 #include "backend/helper/ai_model_parser/ai_model_parser.hpp"
