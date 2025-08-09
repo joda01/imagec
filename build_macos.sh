9d04b70ba407b4032b4faab48cdc23bf40894907
Joachim Danmayr <joachim@danmayr.org>
2025-08-09 17:11:19 +0000

    chore: Copy recursive

--- build_macos.sh
+++ build_macos.sh
@@ -35,7 +35,7 @@ fetch_external_libs() {
     conan remote add imageclibs https://imagec.org:4431/artifactory/api/conan/imageclibs
     conan remote login imageclibs writer -p $CONAN_IMAGEC_ARTIFACTORY_PW
 
-    cp "$WORKING_DIR/conan/profile_macos" /root/.conan2/profiles/default
+    cp -r "$WORKING_DIR/conan/profile_macos" /root/.conan2/profiles/default
 
     conan install . \
       --profile "$WORKING_DIR/conan/profile_macos" \
